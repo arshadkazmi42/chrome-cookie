0480533d0415c9d0222f90269f1b04e9cef435ec
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-05 23:46:07 +0530

    Chrome get query, get cookie path

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-// Initial Function Here
-// This will be entry point of the library
+const SQLiteCrud = require('sqlite3-promisify');
+const { ValuesPointer, SelectFields, QueryValues } = require('genql');
 
-const dummy = () => {
-    // Your code here
+const { ChromeCookiePath, Util } = require('./lib');
+
+
+function Chrome() {
+  this.tableName = 'cookies';
+  this.cookies = require('./cookie.json');
+  this.fields =  SelectFields.get(this.cookies);
+  this.sqliteCrud = new SQLiteCrud(ChromeCookiePath.get());
 }
 
 
-// Export the functions
-module.exports = dummp;
+Chrome.prototype.setCookie = async function(cookies) {
+  const columnsCount = this.cookies.columns.length;
+  const insertQuery = `INSERT INTO ${this.tableName} (${this.fields.join(', ')}) VALUES (${ValuesPointer.get(columnsCount)})`;
+
+  const queries = []
+  for (let cookie of cookies) {
+    const values = QueryValues.get(this.cookies, cookie);
+    queries.push(this.sqliteCrud.run(insertQuery, values));
+  }
+
+  return await Promise.all(queries);
+}
+
+
+Chrome.prototype.getCookie = async function(domain) {
+  const query = `SELECT * from ${this.tableName} WHERE host_key like '%${domain}%'`;
+  return Util.parse(this.cookies, await this.sqliteCrud.all(query));
+}
+
+
+module.exports = Chrome;
